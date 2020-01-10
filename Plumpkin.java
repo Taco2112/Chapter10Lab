efa9cd4a3111afe47bf9256e7e8f095c70492e25
TACO2112 <45360167+Taco2112@users.noreply.github.com>
2020-01-09 22:23:28 -0500

    hiroshima
    When the supernatural snowman falls into a splat of ICEE ,Plumpkin clouds forms destroying all squid life leaving the squid memes in the dusk in till dawn back on wiibowling.com (the national internet graveyard).#unblockwiibowling.com (https://youtu.be/czfUZxSWUnc)
    Plumpkin== 🎃

---
+++ Plumpkin.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.applet.*;
+
+
+public class Plumpkin extends Applet
+{
+	public void paint(Graphics g)
+	{
+		Pumpkin p = new Pumpkin(g);
+	}
+}
+
+
+class Pumpkin
+{
+	public Pumpkin(Graphics g)
+	{
+		drawPumpkin(g);
+	}
+
+	public void drawPumpkin(Graphics g)
+	{
+		g.setColor(Color.orange);
+		g.fillOval(100,100,600,450);
+		g.setColor(new Color(50,200,50));
+		g.fillRect(390,30,20,80);
+	}
+}
